b54841df4dbf92a7d1c27d434a4f8967ae7b5ff8
Bhagatsakshi <142607822+Bhagatsakshi@users.noreply.github.com>
2025-03-24 06:45:36 +0000

    Commit

---
+++ Rhombus.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            printf(" ");
+        }
+        for (int k=0;k<n;k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
